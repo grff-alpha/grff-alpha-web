7d08b9f7d66ecf95cc3e65409ca4797e31bd0fdc
ejairngiraffe <jgarcia@giraffeideas.com>
2014-09-22 09:25:11 -0500

    Checkpoint Header

--- wp-content/themes/base-giraffe/trabajecongiraffe.php
+++ wp-content/themes/base-giraffe/trabajecongiraffe.php
@@ -8,62 +8,72 @@ get_header()
 ?>
 
 	<header class="Header-job">
-		<h3 class="Header-job--title -u-black-bold">Ven y descubre cómo alcanzar las más <span class="-u-red-bold">altas ideas</span></h3>
-		<p class="Header-job--description">En Giraffe nos especializamos en desarrollo de marca,</p>
-		<p class="Header-job--description">consecución de prospectos calificados y producción de herramientas explicativas.</p>
-		<p class="Header-job--description -u-red-bold">#TrabajoSíHay</p>
-		<p class="Header-job--description">Somos un equipo talentoso que disfruta lo que hace:</p>
-		<p class="Header-job--description -u-gray-light-bold">¿Tienes experiencia en alguno de estos cargos?</p>
-		<p class="Header-job--description -u-gray-bold">¡Aplica!</p>
+		<section class="Header-job--titleOne">
+			<h3 class="Header-job--title -u-black-bold">Ven y descubre cómo alcanzar las más <span class="-u-red-bold">altas ideas</span></h3>
+			<p class="Header-job--description -u-gray-bold">En Giraffe nos especializamos en desarrollo de marca,</p>
+			<p class="Header-job--description -u-gray-bold">consecución de prospectos calificados y producción de herramientas explicativas.</p>
+		</section>
+		<section class="Header-job--titleTwo">
+			<p class="Header-job--description Header-job--hashtag -u-red-bold">#TrabajoSíHay</p>
+			<p class="Header-job--description -u-gray-bold">Somos un equipo talentoso que disfruta lo que hace:</p>
+		</section>
+		<section class="Header-job--titleThree">
+			<p class="Header-job--description -u-gray-light-bold">¿Tienes experiencia en alguno de estos cargos?</p>
+			<p class="Header-job--description -u-gray-bold">¡Aplica!</p>
+		</section>
 	</header>
 
-	<section class="Charge-job">
+	<section class="Charge-job-content">
 
-		<section class="Charge-job-main">
-			<h3 class="Charge-job--title -u-orange-bold">Web/UI/Designer</h3>
-			<p class="Charge-job--description">Artista visual experto en branding y <span class="-u-gray-bold">diseño web.</span></p>
-			<p class="Charge-job--description">Con estilo único e impecable.</p>
+		<section class="Charge-job">
+
+			<section class="Charge-job-main Charge-job-mainOne">
+				<h3 class="Charge-job--title -u-orange-bold">Web/UI/Designer</h3>
+				<p class="Charge-job--description">Artista visual experto en branding y <span class="-u-gray-bold">diseño web.</span></p>
+				<p class="Charge-job--description">Con estilo único e impecable.</p>
+			</section>
+
+			<section class="Charge-job-main Charge-job-mainTwo">
+				<h3 class="Charge-job--title -u-orange-bold">Web Developer</h3>
+				<p class="Charge-job--description">Amante del <span class="-u-gray-bold">código.</span></p>
+				<p class="Charge-job--description">Con experiencia en 
+					<span class="-u-gray-bold">CMS:</span> Wordpress
+					<span class="-u-gray-bold">Maquetación:</span> HTML5, CSS3
+					<span class="-u-gray-bold">Lenguajes:</span> Javascript y PHP
+					<span class="-u-gray-bold">Framework:</span> Jquery.
+				</p>
+			</section>
+
 		</section>
 
-		<section class="Charge-job-main">
-			<h3 class="Charge-job--title -u-orange-bold">Web Developer</h3>
-			<p class="Charge-job--description">Amante del <span class="-u-gray-bold">código.</span></p>
-			<p class="Charge-job--description">Con experiencia en 
-				<span class="-u-gray-bold">CMS:</span> Wordpress
-				<span class="-u-gray-bold">Maquetación:</span> HTML5, CSS3
-				<span class="-u-gray-bold">Lenguajes:</span> Javascript y PHP
-				<span class="-u-gray-bold">Framework:</span> Jquery.
-			</p>
+		<section class="Charge-job">
+
+			<section class="Charge-job-main Charge-job-mainThree">
+				<h3 class="Charge-job--title -u-orange-bold">Content Creator</h3>
+				<p class="Charge-job--description">Capaz de generar contenidos de valor y auténticos conceptos de comunicación que <span class="-u-gray-bold">enamoran a la comunidad</span> entorno a la marca.</p>
+			</section>
+
+			<section class="Charge-job-main Charge-job-mainFour">
+				<h3 class="Charge-job--title -u-orange-bold">Motion Graphics Illustrator</h3>
+				<p class="Charge-job--description">Capaz de generar narrativas creativas que aumenten el <span class="-u-gray-bold">entendimiento de las personas.</span></p>
+			</section>
+
 		</section>
 
-	</section>
+		<section class="Charge-job">
 
-	<section class="Charge-job">
+			<section class="Charge-job-main Charge-job-mainFive">
+				<h3 class="Charge-job--title -u-orange-bold">Animator</h3>
+				<p class="Charge-job--description">Artista capaz de dar <span class="-u-gray-bold">vida</span> y movimiento a una <span class="-u-gray-bold">marca.</span></p>
+			</section>
 
-		<section class="Charge-job-main">
-			<h3 class="Charge-job--title -u-orange-bold">Content Creator</h3>
-			<p class="Charge-job--description">Capaz de generar contenidos de valor y auténticos conceptos de comunicación que <span class="-u-gray-bold">enamoran a la comunidad</span> entorno a la marca.</p>
+			<section class="Charge-job-main Charge-job-mainSix">
+				<h3 class="Charge-job--title -u-orange-bold">Sales Executive</h3>
+				<p class="Charge-job--description">Con mínimo 3 años de experiencia coordinando proyectos digitales y de <span class="-u-gray-bold">comunicación de marca.</span> Líder, carismático, ordenado y disciplinado.</p>
+			</section>
+
 		</section>
-
-		<section class="Charge-job-main">
-			<h3 class="Charge-job--title -u-orange-bold">Motion Graphics Illustrator</h3>
-			<p class="Charge-job--description">Capaz de generar narrativas creativas que aumenten el <span class="-u-gray-bold">entendimiento de las personas.</span></p>
-		</section>
-
-	</section>
-
-	<section class="Charge-job">
-
-		<section class="Charge-job-main">
-			<h3 class="Charge-job--title -u-orange-bold">Animator</h3>
-			<p class="Charge-job--description">Artista capaz de dar <span class="-u-gray-bold">vida</span> y movimiento a una <span class="-u-gray-bold">marca.</span></p>
-		</section>
-
-		<section class="Charge-job-main">
-			<h3 class="Charge-job--title -u-orange-bold">Sales Executive</h3>
-			<p class="Charge-job--description">Con mínimo 3 años de experiencia coordinando proyectos digitales y de <span class="-u-gray-bold">comunicación de marca.</span> Líder, carismático, ordenado y disciplinado.</p>
-		</section>
-
+		
 	</section>
 
 	<script charset="utf-8" src="//js.hsforms.net/forms/current.js"></script>
@@ -73,6 +83,10 @@ get_header()
 			formId: '06426a02-420a-4a83-bd9e-800703c1a638'
 		});
 	</script>
+
+	<section class="Message-job">
+		<h2 class="Message-job--title -u-red-bold">! Haz parte de la manada ¡</h2>
+	</section>
 	
 
 <?php get_footer()?>
